f350af7e26a8462077c382c3bb78fd0f2877deea
Jayasurya Gunasekharan <jayasurya.guna@gmail.com>
2024-06-30 01:09:20 -0500

    Updated github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import "./App.css";
-import {
-  FormControl,
-  InputGroup,
-  Container,
-  Button,
-  Card,
-  Row,
-} from "react-bootstrap";
+import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 const clientId = import.meta.env.VITE_CLIENT_ID;
@@ -15,7 +8,6 @@ const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
-  const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
     let authParams = {
@@ -29,7 +21,6 @@ function App() {
         "&client_secret=" +
         clientSecret,
     };
-
     fetch("https://accounts.spotify.com/api/token", authParams)
       .then((result) => result.json())
       .then((data) => {
@@ -56,17 +47,8 @@ function App() {
         return data.artists.items[0].id;
       });
 
-    // Get Artist Albums
-    await fetch(
-      "https://api.spotify.com/v1/artists/" +
-        artistID +
-        "/albums?include_groups=album&market=US&limit=50",
-      artistParams
-    )
-      .then((result) => result.json())
-      .then((data) => {
-        setAlbums(data.items);
-      });
+    console.log("Search Input: " + searchInput);
+    console.log("Artist ID: " + artistID);
   }
 
   return (
@@ -93,76 +75,9 @@ function App() {
               paddingLeft: "10px",
             }}
           />
+
           <Button onClick={search}>Search</Button>
         </InputGroup>
-      </Container>
-
-      <Container>
-        <Row
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "space-around",
-            alignContent: "center",
-          }}
-        >
-          {albums.map((album) => {
-            return (
-              <Card
-                key={album.id}
-                style={{
-                  backgroundColor: "white",
-                  margin: "10px",
-                  borderRadius: "5px",
-                  marginBottom: "30px",
-                }}
-              >
-                <Card.Img
-                  width={200}
-                  src={album.images[0].url}
-                  style={{
-                    borderRadius: "4%",
-                  }}
-                />
-                <Card.Body>
-                  <Card.Title
-                    style={{
-                      whiteSpace: "wrap",
-                      fontWeight: "bold",
-                      maxWidth: "200px",
-                      fontSize: "18px",
-                      marginTop: "10px",
-                      color: "black",
-                    }}
-                  >
-                    {album.name}
-                  </Card.Title>
-                  <Card.Text
-                    style={{
-                      color: "black",
-                    }}
-                  >
-                    Release Date: <br /> {album.release_date}
-                  </Card.Text>
-                  <Button
-                    href={album.external_urls.spotify}
-                    style={{
-                      backgroundColor: "black",
-                      color: "white",
-                      fontWeight: "bold",
-                      fontSize: "15px",
-                      borderRadius: "5px",
-                      padding: "10px",
-                    }}
-                  >
-                    Album Link
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </Row>
       </Container>
     </>
   );
